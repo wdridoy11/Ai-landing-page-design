f6ed916427e6756d07ad66a0f278f22aa564cf28
Ridoy Sharif <developerridoy11@gmail.com>
2025-05-06 10:42:19 +0600

    react and tailwind css install successful

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-blue-500 text-white p-4 rounded-lg">
+  Hello Tailwind!
+</div>
+    </div>
+  )
+}
+
+export default App
